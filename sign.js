92a7265678686d6fddf49cdf12404c4abbc9a4e3
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-27 15:02:50 +0100

    fixed

--- sign.js
+++ sign.js
@@ -8,7 +8,7 @@ function sign(num){
     }
 }
 function sameSign(num1, num2){
-    if (num1 <  0 && num2 < 0 || num1 > 0 && num2 > 0){
+    if (num1 <  0 && num2 < 0 || num1 > 0 && num2 > 0 || num1 === 0 && num2 === 0){
         return true
     }
     return false
