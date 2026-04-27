bc3026e4259817ab657b69fa4d4c383cdcb5ef22
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-27 10:45:31 +0100

    fixed

--- more-or-less.js
+++ more-or-less.js
@@ -1,12 +1,14 @@
-function more(x){
-    return x+1
+let a = 0;
+let b = 0;
+function more(a){
+    return a+1
 }
-function less(x){
-    return x-1
+function less(a){
+    return a-1
 }
-function add(x, v){
-    return x+v
+function add(a, b){
+    return a+b
 }
-function lesubss(x,v){
-    return x-v
+function lesubss(a,b){
+    return a-b
 }
