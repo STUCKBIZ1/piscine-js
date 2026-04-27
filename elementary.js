9b4047fa2aebcb24255f2f0aecdb56329645e550
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-27 21:15:24 +0100

    fixed

--- elementary.js
+++ elementary.js
@@ -8,3 +8,38 @@ function multiply(a, b){
     }
     return result
 }
+function divide(a, b){
+    let sign = 1;
+    let count = 0;
+    if (a < 0){
+        a = -a
+        sign = -sign
+    }
+    if (b < 0){
+        b = -b
+        sign = -sign
+    }
+    while (a >= b){
+       a -= b;
+       count++ 
+    }
+    return count * sign
+}
+function modulo(a, b){
+    let sign = 1;
+    let count = 0;
+    if (a < 0){
+        a = -a
+        sign = -sign
+    }
+    if (b < 0){
+        b = -b
+        sign = -sign
+    }
+    while (a >= b){
+       a -= b;
+       count++ 
+    }
+    return a * sign
+}
+console.log(modulo(12, -7))
