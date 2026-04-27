bd236ae4c2a9930959b90617e1456b0435054880
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-27 14:33:14 +0100

    fixed

---
+++ method-man.js
@@ -0,0 +1,15 @@
+function words(str){
+    return str.split(' ')
+}
+function sentence(array){
+    return array.join(' ')
+}
+function yell(str){
+    return str.toUpperCase();
+}
+function whisper(str){
+    return "*"+str.toLowerCase()+"*"
+}
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
