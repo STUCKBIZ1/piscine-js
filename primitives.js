f347a5419978ed75b8a0ea0a6fb12b6b91dceb11
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-27 09:53:32 +0100

    fixed the consts

--- primitives.js
+++ primitives.js
@@ -1,4 +1,16 @@
-const str = 'hello';
-const num = 0;
-const bool = true;
-const undef = undefined;
+const escapeStr = `\'\\/"'`;
+const arr = [4, '2'];
+const obj = {
+    str : "hello",
+    obj : 0,
+    bool : true,
+    undef: undefined,
+}
+const nested = {
+    arr : [4, undefined, '2'],
+    obj : {
+        str : 'hello',
+        num : 9,
+        bool : true,
+    }
+}
