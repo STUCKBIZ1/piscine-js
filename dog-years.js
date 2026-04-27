3af8d333a012d238108bc8537169a8988b871d85
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-27 13:23:29 +0100

    fixed

--- dog-years.js
+++ dog-years.js
@@ -13,5 +13,5 @@ function dogYears(planet, seconds) {
         neptune: 164.79132
     };
 
-    return earthYears / orbital[planet] * 7;
+    return Number(earthYears / orbital[planet] * 7).toFixed(2);
 }
