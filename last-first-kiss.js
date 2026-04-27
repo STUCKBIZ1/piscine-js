b002a792a739457fba0574da81d21e49ac94bd96
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-27 11:11:21 +0100

    fixed

---
+++ last-first-kiss.js
@@ -0,0 +1,12 @@
+function first(x){
+    return x[0]
+}
+function last(x){
+    return x[x.length-1]
+}
+function kiss(x){
+    let result = [];
+    result.push(x[0])
+    result.push(x[x.length-1])
+    return result
+}
