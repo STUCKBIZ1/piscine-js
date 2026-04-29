42fbe1b4ce322c23f7b12396be39f28f5d73393d
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-29 11:31:38 +0100

    fixed

--- cut-corners.js
+++ cut-corners.js
@@ -64,8 +64,8 @@ function trunc(n){
     }
     return int
 }
-const nums = [3, -3, 3, -3, 0]
-console.log(nums.map(round))
-console.log(nums.map(floor))
-console.log(nums.map(trunc))
-console.log(nums.map(ceil))
+// const nums = [3, -3, 3, -3, 0]
+// console.log(nums.map(round))
+// console.log(nums.map(floor))
+// console.log(nums.map(trunc))
+// console.log(nums.map(ceil))
