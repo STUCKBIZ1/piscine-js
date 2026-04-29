c14f85438b6fab3eb82ed3dd16730f35945e1fbc
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-29 17:43:27 +0100

    fixed

--- pyramid.js
+++ pyramid.js
@@ -1,20 +1,18 @@
-function pyramid(str, n){
+function pyramid(str, n) {
     let result = "";
-    let steps = 0;
-    let g = n -1
-    for(let i = 1; i <= n; i++){
-        for (let k = 1; k <= g; k++){
-                result += " ";
-            }
-        for (let j = 1; j <= i+steps ; j++){
-            result += str
+
+    for (let i = 1; i <= n; i++) {
+        for (let k = 1; k <= n - i; k++) {
+            result += " ";
         }
-        if (i != n){
-            result += "\n"
+        for (let j = 1; j <= (2 * i - 1); j++) {
+            result += str;
         }
-        g--
-        steps++
+        if (i !== n) {
+            result += "\n";
+        }
     }
-    return result
+
+    return result;
 }
-// console.log(pyramid("$", 5))
+//  console.log(pyramid('{}', 12))
