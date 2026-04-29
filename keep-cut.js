f6e3924cf428ab28b36987a4995ac44373da2ef4
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-29 10:35:49 +0100

    fixed

--- keep-cut.js
+++ keep-cut.js
@@ -26,6 +26,9 @@ function cutFirstLast(str){
 function keepFirst(str){
     let result = "";
     let maxichars = 2;
+    if (str.length < maxichars || str == ""){
+        maxichars = str.length
+    }
     for (let i = 0; i < maxichars; i++){
             result += str[i]
     }
@@ -34,6 +37,9 @@ function keepFirst(str){
 function keepLast(str){
     let result = "";
     let startindex = str.length-2;
+    if (str.length === 1 || str === ""){
+        return str
+    }
     for (let i = startindex; i < str.length; i++){
             result += str[i]
     }
@@ -41,6 +47,9 @@ function keepLast(str){
 }
 function keepFirstLast(str){
     let result = "";
+    if (str.length < 4){
+        return str
+    }
     let maxichars = 2
     let startindex = str.length-2;
     for (let i = 0; i < maxichars; i++){
@@ -51,4 +60,4 @@ function keepFirstLast(str){
     }
     return result
 }
-// console.log(keepFirstLast(""))
+console.log(keepFirstLast("asaad"))
