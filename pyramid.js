69ab43c11777bf00563a902c887bd9d5540c5710
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-29 17:53:41 +0100

    fixed

--- pyramid.js
+++ pyramid.js
@@ -1,10 +1,13 @@
 function pyramid(str, n) {
     let result = "";
+    const len = str.length;
 
     for (let i = 0; i < n; i++) {
 
         for (let k = 0; k < n - i - 1; k++) {
-            result += " ";
+            for (let s = 0; s < len; s++) {
+                result += " ";
+            }
         }
 
         for (let j = 0; j < 2 * i + 1; j++) {
