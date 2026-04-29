9e3f5a9220808a8bc52766406d547eab5e525498
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-29 11:20:22 +0100

    fixed

--- cut-corners.js
+++ cut-corners.js
@@ -6,25 +6,13 @@ function round(n){
         x--
         int++
       }
+      return (x >= -0.5)? int: int + 1
     }else{
         while(x <= -1){
             x++
             int--
         }
-    }
-    let diff = n - int
-    if (n >= 0){
-        if (diff >= 0.5){
-            return int+1
-        }else{
-            return int
-        }
-    }else{
-        if(diff <= -0.5){ 
-            return int-1
-        }else{
-            return int
-        }
+        return (x <= -0.5)? int : int-1
     }
 }
 function ceil(n){
@@ -35,17 +23,14 @@ function ceil(n){
             int++
             x--
         }
+        return (n === 0)? int : int+1
     }else{
-        while(x < 0){
+        while(x <= -1){
             int--
             x++
         }
+        return int
     }
-    let diff = n - int
-    if (diff == 0){
-        return n
-    }
-    return int + 1
 }
 function floor(n){
     let int = 0;
@@ -61,14 +46,7 @@ function floor(n){
             x++
         }
     }
-    let diff = n - int
-    if (diff == 0){
-        return int
-    }
-    if (n < 0){
-        return int-1
-    }
-    return int
+    return (x === 0)? int : int -1
 }
 function trunc(n){
     let int = 0;
@@ -86,8 +64,8 @@ function trunc(n){
     }
     return int
 }
-// const nums = [3.7, -3.7, 3.1, -3.1]
-// console.log(nums.map(round))
-// console.log(nums.map(floor))
-// console.log(nums.map(trunc))
-// console.log(nums.map(ceil))
+const nums = [3.7, -3.7, 3.1, -3.1]
+console.log(nums.map(round))
+console.log(nums.map(floor))
+console.log(nums.map(trunc))
+console.log(nums.map(ceil))
