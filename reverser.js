a6baa5723c5e3d5d713206c56be2bc3ef0a23db5
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-29 13:29:24 +0100

    fixed

--- reverser.js
+++ reverser.js
@@ -1,8 +1,13 @@
 function reverse(ars){
-    let arr = [];
+    let arr = (typeof ars ==='string')? "": [];
     for (let i = ars.length-1; i >= 0; i--){
+        if (typeof ars === 'string'){
+            arr += ars[i]
+        }else{
         arr.push(ars[i])
+            
+        }
     }
     return arr
 }
-// console.log(reverse([]))
+console.log(reverse([]))
