3d9de0ec3d4cb405d72e33f76a496a4d240b481c
aelhassn <hassnaouiabdelali89@gmail.com>
2026-04-29 10:04:52 +0100

    fixed

--- index-of.js
+++ index-of.js
@@ -6,9 +6,10 @@ function indexOf(arr, value, start = 0){
     }
     return -1
 }
+function lastIndexOf(arr, value, start = arr.length - 1){
+    if (start >= arr.length) start = arr.length - 1
 
-function lastIndexOf(arr , value){
-    for (let i = arr.length - 1; i >= 0; i--){
+    for (let i = start; i >= 0; i--){
         if (arr[i] === value){
             return i
         }
