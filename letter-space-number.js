16eb73f054a69de3b5736660d90c3b58f5f4c12d
aelhassn <hassnaouiabdelali89@gmail.com>
2026-05-01 12:02:07 +0100

    fixed

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,3 +1,4 @@
 function letterSpaceNumber(str ) {
-    return str.match(/[a-zA-Z] [0-9](?![a-zA-Z])/g) || []
+    return str.match(/[a-zA-Z] [0-9](?![a-zA-Z0-9])/g) || []
 }
+console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'))
