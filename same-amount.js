7106d87832bc9d8433adcfde6a6ec51de2ab2487
aelhassn <hassnaouiabdelali89@gmail.com>
2026-05-01 16:25:33 +0100

    fixed

--- same-amount.js
+++ same-amount.js
@@ -1,4 +1,6 @@
 function sameAmount(str, r, r1){
+    r = new RegExp(r.source, r.flage + 'g')
+    r1 = new RegExp(r1.source, r1.flage + 'g')  
     const count1 = (str.match(r) || []).length
     const count2 = (str.match(r1) || []).length
     return count1 === count2
