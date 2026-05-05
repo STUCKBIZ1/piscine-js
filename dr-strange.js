feb84a4e2ef9d44eefe009a6d4d35ea6cd2f4b6c
abdelali <hassnaouiabdelali89@gmail.com>
2026-05-05 18:30:00 +0100

    fixed

---
+++ dr-strange.js
@@ -0,0 +1,35 @@
+let week = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",
+"secondMonday", "secondTuesday", "secondWednesday", "secondThursday",
+"secondFriday", "secondSaturday", "secondSunday"
+]
+const e = new Date("0001-01-01")
+
+function addWeek(date){
+   const diff = date - e
+   let days =Math.floor(diff/(1000 * 60 * 60 * 24))
+   let day = days%14
+return week[day]
+}
+function timeTravel({ date, hour, minute, second }) {
+    return new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate(),
+        hour,
+        minute,
+        second
+    );
+}
+// console.log(timeTravel({
+//   date: new Date("2020-05-29 23:25:22"),
+//   hour: 21,
+//   minute: 22,
+//   second: 22,
+// }).toString())
+
+// addWeek(new Date("0001-01-01")); // Output: 'Monday'
+// addWeek(new Date("0001-01-02")); // Output: 'Tuesday'
+// addWeek(new Date("0001-01-07")); // Output: 'Sunday'
+// addWeek(new Date("0001-01-08")); // Output: 'secondMonday'
+// addWeek(new Date("0001-01-09")); // Output: 'secondTuesday'
