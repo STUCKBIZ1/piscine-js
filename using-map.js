94756324d79875453efea9764631e77a75b082f0
abdelali <hassnaouiabdelali89@gmail.com>
2026-05-06 17:09:03 +0100

    fixed

--- using-map.js
+++ using-map.js
@@ -27,9 +27,9 @@ function tocap(strs){
     return re1.join(" ")
 }
 function fahrenheitToCelsius(arr){
-    return arr.map(temp =>{ 
-        let num = Number(temp.replace("°F", ""))
-        let c = Math.round((num - 32) * 5/9);
+    return arr.map(temp => { 
+        let num = Number(temp.replace("°F", "").trim())
+        let c = Math.floor((num - 32) * 5 / 9)
         return c + "°C"
     })
 }
@@ -45,17 +45,17 @@ function trimTemp(objs){
 // ]))
 function tempForecasts(objs){
     return objs.map(obj => {
-        let temp = obj["temperature"].match(/\d/g)
-        obj["state"] = obj["state"][0].toUpperCase()+obj["state"].slice(1).toLowerCase();
-        temp = Number(temp.join(''))
-        temp = String(Math.round((temp-32)*5/9))+"°Celsius in "+obj["city"]+', '+obj["state"]
-        return temp
+        let temp = obj["temperature"].replace("°F", "")
+        let stat = obj["state"].split(' ').map(s => s[0].toUpperCase()+s.slice(1).toLowerCase()).join(' ');
+        temp = Number(temp)
+        temp =  Math.floor((temp - 32) * 5 / 9)
+        return String(temp+"°Celsius in "+obj["city"]+", "+stat)
     })
 }
 // console.log(tempForecasts([
 //   {
 //     city: 'Pasadena',
-//     temperature: ' 101 °F',
+//     temperature: ' -1 °F',
 //     state: 'california',
 //     region: 'West',
 //   },
