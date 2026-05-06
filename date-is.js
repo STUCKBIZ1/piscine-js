52f3b77297b13876b731b1a531baf532202082fc
abdelali <hassnaouiabdelali89@gmail.com>
2026-05-06 01:14:03 +0100

    fixed

--- date-is.js
+++ date-is.js
@@ -1,21 +1,26 @@
 function isValid(date){
-    let time = date.getTime()
-    if (Number.isNaN(time)){
+     if (typeof date === "number") {
+        return !Number.isNaN(date);
+    }
+    if (!(date instanceof Date)){
         return false
     }
-    return true
+    return !Number.isNaN(date.getTime())
 }
 function isAfter(date1, date2){
     if (!isValid(date1) || !isValid(date2)){
         return false
     }
-    let time1 = date1.getTime();
-    let time2 = date2.getTime();
+    let time1 = ToTime(date1)
+    let time2 = ToTime(date2)
     return time1 > time2 
 }
 function isBefore(date1, date2){
-    let time1 = date1.getTime();
-    let time2 = date2.getTime();
+    if (!isValid(date1) || !isValid(date2)){
+        return false
+    }
+    let time1 = ToTime(date1)
+    let time2 = ToTime(date2)
     return time1 < time2;
 }   
 function isFuture(date){
@@ -26,7 +31,7 @@ function isFuture(date){
     if (date.getTime() > now.getTime()){
         return true
     }
-    return true
+    return false
     
 }
 function isPast(date){
@@ -37,7 +42,14 @@ function isPast(date){
     if (date.getTime() < now.getTime()){
         return true
     }
-    return true
+    return false
     
+}
+function ToTime(date){
+    if (date instanceof Date){
+        return date.getTime()
+    }else if (typeof date === "number"){
+        return date
+    }
 }
 // console.log(isBefore(new Date("2004-01-02"), new Date("2004-01-02")))
