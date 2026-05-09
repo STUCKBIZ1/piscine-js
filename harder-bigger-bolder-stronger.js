5db6c0ea3a022fee4d7ea28d57f6f57e0bbd238b
aelhassn <hassnaouiabdelali89@gmail.com>
2026-05-09 12:34:26 +0100

    fixed

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,18 @@
+export function generateLetters(){
+    let alphaapper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let total = "120";
+    for (let i = 1; i <= total; i++){
+        let div = document.createElement("div")
+        div.textContent = alphaapper[Math.floor(Math.random()*alphaapper.length)]
+        let fontsize = i+11;
+        div.style.fontSize = fontsize+"px";
+        if (i < total / 3){
+            div.style.fontWeight = 300
+        }else if(i < (total/3)*2){
+            div.style.fontWeight = 400
+        }else{
+            div.style.fontWeight = 600
+        }
+        document.body.append(div)
+    }
+}
