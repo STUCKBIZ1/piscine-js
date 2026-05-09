3b19420c073f7530ae84b3f9df537f5ec556bc09
aelhassn <hassnaouiabdelali89@gmail.com>
2026-05-09 14:43:10 +0100

    fixed

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,24 +1,21 @@
-
 import {styles} from './pimp-my-style.data.js'
 let index = 0;
 let action = "add";
+
 export function pimp(){
     let btn = document.querySelector(".button")
     if (action === "add"){
-       btn.classList.add(styles[index])
-       index++
-       if (index === styles.length){
-        btn.classList.add("unpimp")
-        action = "remove"
-       }
-       
-    }else{
+        btn.classList.add(styles[index])
+        index++
         if (index === styles.length){
-        btn.classList.remove("unpimp")
-       }
-        btn.classList.remove(styles[index-1])
+            btn.classList.add("unpimp")
+            action = "remove"
+        }
+    } else {
+        btn.classList.remove(styles[index - 1])
         index--
         if (index === 0){
+            btn.classList.remove("unpimp")
             action = "add"
         }
     }
