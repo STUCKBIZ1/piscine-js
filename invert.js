ef8d51b2b952fe393ec71b3ca29c5d58bb0318e2
aelhassn <hassnaouiabdelali89@gmail.com>
2026-05-11 18:35:38 +0100

    fixed

--- invert.js
+++ invert.js
@@ -2,11 +2,12 @@ function invert(obj){
     let newobj = {};
     for (let key in obj){
         if (typeof key === "object"){
-            newobj[obj[key]] = invert(key)
+            // newobj[obj[key]] = invert(key)
+            continue
         }else{
              newobj[obj[key]] = key;
         }
     }
     return newobj
 }
-// console.log(invert({a : 'b', b : 'a'}))
+//  console.log(invert({ f: 5, __proto__: { d: 6 } }))
