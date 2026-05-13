04043955a66019aeb4a62de4bf4d8183a3fc7987
abdelali <hassnaouiabdelali89@gmail.com>
2026-05-13 19:27:31 +0100

    fixed

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -24,10 +24,11 @@
 
     return Math.round(total * 10) / 10
     }
-function lowCarbs(cart) {
+
+  function lowCarbs(cart) {
   return filterEntries(cart, ([item, grams]) => {
     const carbsPer100 = nutritionDB[item].carbs
-    const totalCarbs = (carbsPer100 * grams) / 100
+    const totalCarbs = carbsPer100 * grams / 100
     return totalCarbs < 50
   })
 }
@@ -46,3 +47,13 @@ function lowCarbs(cart) {
         return [item, scaled]
     })
     }
+    const nutritionDB = {
+  tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+  vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+  oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+  onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+  garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+  paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+  sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+  orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
+}
