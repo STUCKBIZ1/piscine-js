e377bb5c2f8a02ce001a183902f29c614e6bf464
abdelali <hassnaouiabdelali89@gmail.com>
2026-05-13 19:41:05 +0100

    fixed

--- replica.js
+++ replica.js
@@ -1,7 +1,9 @@
 function replica(target, ...sources) {
   for (const source of sources) {
     for (const key in source) {
-      if (Array.isArray(source[key])) {
+      if (source[key] instanceof RegExp) {
+        target[key] = source[key];
+      } else if (Array.isArray(source[key])) {
         target[key] = replica([], source[key]);
       } else if (typeof source[key] === 'object' && source[key] !== null) {
         target[key] = replica({}, source[key]);
