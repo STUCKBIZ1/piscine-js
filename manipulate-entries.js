c4664e8b6d5967ed8c8055b34165d44d33fd984b
abdelali <hassnaouiabdelali89@gmail.com>
2026-05-13 19:09:23 +0100

    fixed

---
+++ manipulate-entries.js
@@ -0,0 +1,47 @@
+function filterEntries(obj, fn) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([k, v]) => fn([k, v]))
+  )
+}
+
+function mapEntries(obj, fn) {
+  return Object.fromEntries(
+    Object.entries(obj).map(([k, v]) => fn([k, v]))
+  )
+}
+
+function reduceEntries(obj, fn, start) {
+  return Object.entries(obj).reduce(
+    (acc, [k, v]) => fn(acc, [k, v]),
+    start
+  )
+}
+
+function totalCalories(cart) {
+  const total = reduceEntries(cart, (total, [item, grams]) => {
+    return total + (nutritionDB[item].calories * grams / 100)
+  }, 0)
+
+  return Math.round(total * 10) / 10
+}
+
+function lowCarbs(cart) {
+  return filterEntries(cart, ([item, grams]) => {
+    return (nutritionDB[item].carbs * grams / 100) < 50
+  })
+}
+
+function cartTotal(cart) {
+  return mapEntries(cart, ([item, grams]) => {
+    const nutrition = nutritionDB[item]
+    const scaled = {}
+
+    for (const key in nutrition) {
+      scaled[key] = Number(
+        (nutrition[key] * grams / 100).toFixed(3)
+      )
+    }
+
+    return [item, scaled]
+  })
+}
